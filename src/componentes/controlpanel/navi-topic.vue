94d8856d97186bb8fe4ab7e2ad424e89ad37a49d
eugen <ed@ipunkt.biz>
2019-06-17 17:17:20 +0200

    Elements added

---
+++ src/componentes/controlpanel/navi-topic.vue
@@ -0,0 +1,149 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col p-0">
+                <nav class="navbar navbar-dark bg-dark">
+                    <info-icon></info-icon>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+            </div>
+        </div>
+        <div class="collapse row" id="navbarToggleExternalContent">
+                <div class="col p-0">
+                    <table class="row-fluid table table-hover">
+                        <thead>
+                        <tr class="bg-dark text-white">
+                            <th scope="col">Sprache</th>
+                            <th scope="col">Selbsteinschätzung</th>
+                            <th scope="col">Erfahrung in Jahren</th>
+                            <th scope="col">Woher kommt die Erfahrung</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">HTML/CSS/SCSS</th>
+                            <td>Experte</td>
+                            <td>19</td>
+                            <td>Schulfach, Umschulungsfach, Selbststudium</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">PHP</th>
+                            <td>Fortgeschritten</td>
+                            <td>5</td>
+                            <td>Umschulungsfach, Selbststudium</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">JavaScript</th>
+                            <td>Fortgeschritten</td>
+                            <td>2</td>
+                            <td>Umschulungsfach, Selbststudium</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Java</th>
+                            <td>Grundkenntnisse</td>
+                            <td>0,5</td>
+                            <td>Studienfach, Selbststudium</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">C+</th>
+                            <td>Grundkenntnisse</td>
+                            <td>0,5</td>
+                            <td>Studienfach</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">C#</th>
+                            <td>Grundkenntnisse</td>
+                            <td>0,5</td>
+                            <td>Umschulungsfach, Selbststudium</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+        </div>
+    </div>
+</template>
+<script>
+    import 'bootstrap/js/dist/collapse.js';
+    import InfoIcon from "./visiting-card";
+    export default {
+        name: "navi-topic",
+        components: {InfoIcon},
+        data() {
+            return {
+                degQuarterList: {
+                    degQuarterStart: 0,
+                    degQuarter1: 0,
+                    degQuarter2: 0,
+                    degQuarter3: 0,
+                    degQuarter4: 0,
+                    degQuarter5: 0,
+                    degQuarter6: 0,
+                    degQuarter7: 0,
+                    degQuarter8: 0,
+                },
+                textQuarterList: {
+                    textQuarter1: "",
+                    textQuarter2: "",
+                    textQuarter3: "",
+                    textQuarter4: "",
+                    textQuarter5: "",
+                    textQuarter6: "",
+                    textQuarter7: "",
+                    textQuarter8: "",
+                },
+            }
+        },
+        methods: {
+            resetDegree: function(degree){
+                if(parseInt(degree) <=25){
+                    return 0;
+                }
+                else {
+                    return parseInt(degree) -25;
+                }
+            },
+            degSum: function() {
+              return parseInt(this.degQuarterList.degQuarter1)+parseInt(this.degQuarterList.degQuarter2)+parseInt(this.degQuarterList.degQuarter3)+parseInt(this.degQuarterList.degQuarter4)+parseInt(this.degQuarterList.degQuarter5)+parseInt(this.degQuarterList.degQuarter6)+parseInt(this.degQuarterList.degQuarter7)+parseInt(this.degQuarterList.degQuarter8);
+            },
+            setDegree: function () {
+                // ||this.degSum()<100
+                if(this.degSum()>100){
+                    alert("DER GESAMTWERT LIEGT ÜBER 100%");
+                }
+                else {
+                    this.degQuarterList.degQuarterStart = parseInt(this.degQuarterList.degQuarterStart);
+                    this.degQuarterList.degQuarter1 = this.resetDegree(this.degQuarterList.degQuarter1) + parseInt(this.degQuarterList.degQuarterStart);
+                    this.degQuarterList.degQuarter2 = parseInt(this.degQuarterList.degQuarter2) + parseInt(this.degQuarterList.degQuarter1);
+                    this.degQuarterList.degQuarter3 = parseInt(this.degQuarterList.degQuarter3) + parseInt(this.degQuarterList.degQuarter2);
+                    this.degQuarterList.degQuarter4 = parseInt(this.degQuarterList.degQuarter4) + parseInt(this.degQuarterList.degQuarter3);
+                    this.degQuarterList.degQuarter5 = parseInt(this.degQuarterList.degQuarter5) + parseInt(this.degQuarterList.degQuarter4);
+                    this.degQuarterList.degQuarter6 = parseInt(this.degQuarterList.degQuarter6) + parseInt(this.degQuarterList.degQuarter5);
+                    this.degQuarterList.degQuarter7 = parseInt(this.degQuarterList.degQuarter7) + parseInt(this.degQuarterList.degQuarter6);
+                    this.degQuarterList.degQuarter8 = parseInt(this.degQuarterList.degQuarter8) + parseInt(this.degQuarterList.degQuarter7);
+                    this.$emit('getInputContent',[this.degQuarterList.degQuarterStart, this.degQuarterList.degQuarter1, this.degQuarterList.degQuarter2, this.degQuarterList.degQuarter3,this.degQuarterList.degQuarter4,this.degQuarterList.degQuarter5,this.degQuarterList.degQuarter6,this.degQuarterList.degQuarter7,this.degQuarterList.degQuarter8])
+                    this.degQuarterList.degQuarter8 = parseInt(this.degQuarterList.degQuarter8) - parseInt(this.degQuarterList.degQuarter7);
+                    this.degQuarterList.degQuarter7 = parseInt(this.degQuarterList.degQuarter7) - parseInt(this.degQuarterList.degQuarter6);
+                    this.degQuarterList.degQuarter6 = parseInt(this.degQuarterList.degQuarter6) - parseInt(this.degQuarterList.degQuarter5);
+                    this.degQuarterList.degQuarter5 = parseInt(this.degQuarterList.degQuarter5) - parseInt(this.degQuarterList.degQuarter4);
+                    this.degQuarterList.degQuarter4 = parseInt(this.degQuarterList.degQuarter4) - parseInt(this.degQuarterList.degQuarter3);
+                    this.degQuarterList.degQuarter3 = parseInt(this.degQuarterList.degQuarter3) - parseInt(this.degQuarterList.degQuarter2);
+                    this.degQuarterList.degQuarter2 = parseInt(this.degQuarterList.degQuarter2) - parseInt(this.degQuarterList.degQuarter1);
+                    this.degQuarterList.degQuarter1 = parseInt(this.degQuarterList.degQuarter1) + 25;
+                    this.$emit('getInputContentText',[this.textQuarterList.textQuarter1, this.textQuarterList.textQuarter2, this.textQuarterList.textQuarter3,this.textQuarterList.textQuarter4,this.textQuarterList.textQuarter5,this.textQuarterList.textQuarter6,this.textQuarterList.textQuarter7,this.textQuarterList.textQuarter8])
+                }
+            },
+            sum: function(){
+                let summe = parseInt(this.degQuarterList.degQuarter1)+parseInt(this.degQuarterList.degQuarter2)+parseInt(this.degQuarterList.degQuarter3)+parseInt(this.degQuarterList.degQuarter4)+parseInt(this.degQuarterList.degQuarter5)+parseInt(this.degQuarterList.degQuarter6)+parseInt(this.degQuarterList.degQuarter7)+parseInt(this.degQuarterList.degQuarter8);
+                return summe;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .w-max {
+        max-width: 45px;
+    }
+</style>
---
+++ src/componentes/controlpanel/visiting-card.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <span class="p-1 text-white h5">Eugen Demidcik</span>
+        <button class="navbar-toggler border bg-dark p-1 btn-group-vertical text-white ml-1" data-toggle="collapse" data-target="#info">?</button>
+        <div class="collapse text-white w-100 position-absolute fixed-top mt-5" id="info">
+                <div class="card mt-3">
+                    <div class="topic text-center w-100 border-bottom border-white p-0">F a c h i n f o r m a t i k e r</div>
+                    <div class="text-center">E u g e n &nbsp;&nbsp; D e m i d c i k<br>Mobil:&nbsp;&nbsp;0176 69558695</div>
+                    <div class="bottom text-right mt-4 p-1">Heppacher Str. 8<br>70327 Stuttgart<br>Mobil:&nbsp;&nbsp;0176 69558695<br>E-Mail: eugen.dem@gmx.de</div>
+                </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "visiting-card"
+    }
+</script>
+
+<style scoped>
+    button {
+        min-width: 18px;
+        max-height: 18px;
+        box-shadow: 0px 0px 2px #fff;
+        font-size: 12px;
+    }
+    .card {
+        width: 				85mm;
+        height:				55mm;
+        background-image:	url(../../img/netz.jpeg);
+        background-size:	85mm 55mm;
+        border:				2px dotted;
+        border-radius:		3mm;
+
+    }
+    .topic {
+        background-color:	rgba(0,102,153,.4);
+        height:				8mm;
+        font-size:			5mm;
+        font-weight:		bold;
+        text-shadow:		-1px 2px 2px rgba(255,255,0,.7);
+        padding:			1mm;
+
+    }
+    .bottom{
+        text-shadow:		-1px 2px 2px rgba(0,0,0,.9);
+    }
+</style>
