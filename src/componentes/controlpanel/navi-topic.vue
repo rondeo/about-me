3cca14a596883ac48dbc0050fc5a3750e9f2de1a
eugen <ed@ipunkt.biz>
2019-06-18 11:16:42 +0200

    table adapted

--- src/componentes/controlpanel/navi-topic.vue
+++ src/componentes/controlpanel/navi-topic.vue
@@ -10,56 +10,9 @@
                 </nav>
             </div>
         </div>
-        <div class="collapse row" id="navbarToggleExternalContent">
+        <div class="collapse row overflow-auto" id="navbarToggleExternalContent">
                 <div class="col p-0">
-                    <table class="row-fluid table table-hover">
-                        <thead>
-                        <tr class="bg-dark text-white">
-                            <th scope="col">Sprache</th>
-                            <th scope="col">Selbsteinschätzung</th>
-                            <th scope="col">Erfahrung in Jahren</th>
-                            <th scope="col">Woher kommt die Erfahrung</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">HTML/CSS/SCSS</th>
-                            <td>Experte</td>
-                            <td>19</td>
-                            <td>Schulfach, Umschulungsfach, Selbststudium</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">PHP</th>
-                            <td>Fortgeschritten</td>
-                            <td>5</td>
-                            <td>Umschulungsfach, Selbststudium</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">JavaScript</th>
-                            <td>Fortgeschritten</td>
-                            <td>2</td>
-                            <td>Umschulungsfach, Selbststudium</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Java</th>
-                            <td>Grundkenntnisse</td>
-                            <td>0,5</td>
-                            <td>Studienfach, Selbststudium</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">C+</th>
-                            <td>Grundkenntnisse</td>
-                            <td>0,5</td>
-                            <td>Studienfach</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">C#</th>
-                            <td>Grundkenntnisse</td>
-                            <td>0,5</td>
-                            <td>Umschulungsfach, Selbststudium</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <p-languages></p-languages>
                 </div>
         </div>
     </div>
@@ -67,9 +20,10 @@
 <script>
     import 'bootstrap/js/dist/collapse.js';
     import VisitingCard from "./visiting-card";
+    import PLanguages from "./p-languages";
     export default {
         name: "navi-topic",
-        components: {VisitingCard},
+        components: {PLanguages, VisitingCard},
         data() {
             return {
             }
---
+++ src/componentes/controlpanel/p-languages.vue
@@ -0,0 +1,60 @@
+<template>
+    <table class="table table-hover">
+        <thead>
+        <tr class="bg-dark text-white">
+            <th scope="col">Sprache</th>
+            <th scope="col">Selbsteinschätzung</th>
+            <th scope="col">Erfahrung in Jahren</th>
+            <th scope="col">Woher kommt die Erfahrung</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">HTML/CSS/SCSS</th>
+            <td>Experte</td>
+            <td>19</td>
+            <td>Schulfach, Umschulungsfach, Selbststudium</td>
+        </tr>
+        <tr>
+            <th scope="row">PHP</th>
+            <td>Fortgeschritten</td>
+            <td>5</td>
+            <td>Umschulungsfach, Selbststudium</td>
+        </tr>
+        <tr>
+            <th scope="row">JavaScript</th>
+            <td>Fortgeschritten</td>
+            <td>2</td>
+            <td>Umschulungsfach, Selbststudium</td>
+        </tr>
+        <tr>
+            <th scope="row">Java</th>
+            <td>Grundkenntnisse</td>
+            <td>0,5</td>
+            <td>Studienfach, Selbststudium</td>
+        </tr>
+        <tr>
+            <th scope="row">C+</th>
+            <td>Grundkenntnisse</td>
+            <td>0,5</td>
+            <td>Studienfach</td>
+        </tr>
+        <tr>
+            <th scope="row">C#</th>
+            <td>Grundkenntnisse</td>
+            <td>0,5</td>
+            <td>Umschulungsfach, Selbststudium</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "p-languages"
+    }
+</script>
+
+<style scoped>
+
+</style>
