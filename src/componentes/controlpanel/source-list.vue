a4c2f021b1e73154183bdc6c83c6a84653b3b67c
eugen <ed@ipunkt.biz>
2019-06-18 11:38:59 +0200

    source-list adapted

--- src/componentes/controlpanel/source-list.vue
+++ src/componentes/controlpanel/source-list.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="collapse text-white w-100 position-absolute fixed-top mt-5" id="source">
         <div class="mt-3 p-2 bg-dark">
-            <a href="https://mathebw.de/" class="text-white">- https://mathebw.de/</a><br>
-            <a href="https://github.com/HPDESIGNER/carousel-css-js-demo" class="text-white">- https://github.com/HPDESIGNER/carousel-css-js-demo</a><br>
-            <a href="https://memory-js.playground.ipunkt.cloud/" class="text-white">- https://memory-js.playground.ipunkt.cloud/</a><br>
-            <a href="https://github.com/HPDESIGNER/memory-php" class="text-white">- https://github.com/HPDESIGNER/memory-php</a><br>
+            <div class="row w-100 m-0 p-0"><div class="col-1 m-0 p-0">*</div><div class="col-11 m-0 p-0"><a href="https://mathebw.de/" class="text-white"> https://mathebw.de/</a></div></div>
+            <div class="row w-100 m-0 p-0"><div class="col-1 m-0 p-0">*</div><div class="col-11 m-0 p-0"><a href="https://github.com/HPDESIGNER/carousel-css-js-demo" class="text-white">GitHub Projekt Carousel mit CSS und JavaScript</a></div></div>
+            <div class="row w-100 m-0 p-0"><div class="col-1 m-0 p-0">*</div><div class="col-11 m-0 p-0"><a href="https://github.com/HPDESIGNER/memory-php" class="text-white">GitHub Projekt Memory Spiel in PHP</a></div></div>
+            <div class="row w-100 m-0 p-0"><div class="col-1 m-0 p-0">*</div><div class="col-11 m-0 p-0"><a href="https://github.com/HPDESIGNER/memory-php" class="text-white">Prüfungsprojekt Memory Spiel mit Vue Komponenten</a></div></div>
         </div>
     </div>
 </template>
